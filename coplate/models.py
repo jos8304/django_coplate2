ce5be83ca4624e7be38bf20a3f122698cf2cd88e
jos8304 <jyou8304@gmail.com>
2024-04-30 20:10:38 -0400

    Add Like model

--- coplate/models.py
+++ coplate/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 from .validators import validate_no_special_characters, validate_restaurant_link
 
@@ -69,3 +70,13 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content[:30]
+
+class Like(models.Model):
+    dt_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    content_type = models.ForeignKey("ContectType", on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    liked_object = GenericForeignKey('content_type','object_id')
+
+    def __str__(self):
+        return f"({self.user}, {self.liked_object})"
