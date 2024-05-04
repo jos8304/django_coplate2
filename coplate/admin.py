68543002a1b52e31658de9a3619660f0cc2a2e56
jos8304 <jyou8304@gmail.com>
2024-05-04 16:39:22 -0400

    Add following user

--- coplate/admin.py
+++ coplate/admin.py
@@ -11,9 +11,14 @@ class CommentInline(admin.StackedInline):
 class LikeInline(GenericStackedInline):
     model = Like
 
+class UserInline(admin.StackedInline):
+    model = User.following.through
+    fk_name = 'to_user'
+    verbose_name = 'Follower'
+    verbose_name_plural = 'Followers'
 
 UserAdmin.fieldsets += ('Custom fields', {'fields': ('nickname', 'profile_pic', 'intro','following')}),
-
+UserAdmin.inlines = (UserInline,)
 
 class ReviewAdmin(admin.ModelAdmin):
     inlines = (
