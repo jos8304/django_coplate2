c918b2db950069c494604b0e5a536037eb086ba9
jos8304 <jyou8304@gmail.com>
2024-05-04 16:32:08 -0400

    Add like

--- coplate/admin.py
+++ coplate/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.contenttypes.admin import GenericStackedInline
 
-from .models import User, Review
+from .models import User, Review, Comment, Like
 
-UserAdmin.fieldsets += ('Custom fields', {'fields': ('nickname', 'profile_pic', 'intro',)}),
+class CommentInline(admin.StackedInline):
+    model = Comment
 
+
+class LikeInline(GenericStackedInline):
+    model = Like
+
+
+UserAdmin.fieldsets += ('Custom fields', {'fields': ('nickname', 'profile_pic', 'intro','following')}),
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+        LikeInline,
+    )
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (        
+        LikeInline,
+    )
+        
 admin.site.register(User, UserAdmin)
 
-admin.site.register(Review)
+admin.site.register(Review,ReviewAdmin)
+
+admin.site.register(Comment,CommentAdmin)
+
+admin.site.register(Like)
--- coplate/models.py
+++ coplate/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 from .validators import validate_no_special_characters, validate_restaurant_link
 
@@ -80,7 +81,7 @@ class Comment(models.Model):
 class Like(models.Model):
     dt_created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey("User", on_delete=models.CASCADE)
-    content_type = models.ForeignKey("ContectType", on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     liked_object = GenericForeignKey('content_type','object_id')
 
