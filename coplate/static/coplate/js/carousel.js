372102c6ab0fca13b9ece1b2268532a890495c91
jos8304 <jyou8304@gmail.com>
2024-05-16 21:22:07 -0400

    thumbnails

--- coplate/static/coplate/js/carousel.js
+++ coplate/static/coplate/js/carousel.js
@@ -2,26 +2,42 @@
     const prevBtn = document.querySelector('.carousel .carousel-button.prev');
     const nextBtn = document.querySelector('.carousel .carousel-button.next');
     const images = document.querySelectorAll('.carousel img.full');
+    const thumbnails = document.querySelectorAll('.carousel img.thumb');
 
 
     let currIdx = 0;
     function handlePrevClick(e){
         if (currIdx > 0){
             images[currIdx].classList.add('hidden');
+            thumbnails[currIdx].classList.add('inactive');
             currIdx--;
             images[currIdx].classList.remove('hidden'); 
+            thumbnails[currIdx].classList.remove('inactive');
         }
     }
 
     function handleNextClick(e){
         if (currIdx < images.length-1){
             images[currIdx].classList.add('hidden');
+            thumbnails[currIdx].classList.add('inactive');
             currIdx++;
-            images[currIdx].classList.remove('hidden'); 
+            images[currIdx].classList.remove('hidden');
+            thumbnails[currIdx].classList.remove('inactive');
         }
         
     }
 
+    function handleThumbnailClick(e){
+        images[currIdx].classList.add('hidden');
+        thumbnails[currIdx].classList.add('inactive');
+        currIdx = e.target.dataset.index;
+        images[currIdx].classList.remove('hidden');
+        thumbnails[currIdx].classList.remove('inactive');        
+    }
+
     prevBtn.addEventListener('click', handlePrevClick)
     nextBtn.addEventListener('click', handleNextClick)
+    for (let thumbnail of thumbnails){
+        thumbnail.addEventListener('click', handleThumbnailClick)
+    }
 }
